a57ba66a7a29ba92624318b4dd40a6a20ac0e22d
Arimafit-mayank <mayank@arimafit.com>
2026-02-07 03:20:27 +0530

    Initial Spring Boot setup with MySQL configuration

---
+++ src/main/java/com/expensesplitter/expense_splitter/ExpenseSplitterApplication.java
@@ -0,0 +1,13 @@
+package com.expensesplitter.expense_splitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseSplitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseSplitterApplication.class, args);
+	}
+
+}
